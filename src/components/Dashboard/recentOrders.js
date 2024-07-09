7cbe739828930b6c0be2e9c90dff899067dd8774
jeetbhalani15 <Jeetbhalani@gmail.com>
2024-07-10 01:24:37 +0530

    updated recent orders card details

--- src/components/Dashboard/recentOrders.js
+++ src/components/Dashboard/recentOrders.js
@@ -6,58 +6,173 @@ const RecentOrders = () => {
     <div className='recent-order-box'>
     <h3>Recent Orders</h3>
         <div class="mt-2">
-    <div class="flex flex-wrap items-center justify-center flex-col">
-      <div class=" px-2 my-2 sm:w-1/2">
-        <div class="items-center shadow-md overflow-hidden rounded-lg bg-white hover:shadow-lg">
-          <div href="#" class="block">
-            <img src="https://i.imgur.com/uzYifbO.jpg" class="w-20 h-20 object-cover block mx-auto"/>
-          </div>
-
-          <div href="#" class="text-left text-base font-normal mt-2 ml-4">
-            Samsung Z Fold
-          </div>
-          <div class="font-bold text-left text-lg mb-1 ml-4">$1,799.00 </div>
-        </div>
-      </div>
-
+    <div class="flex flex-wrap justify-center cursor-pointer flex-col">
       <div class="px-2 my-2 sm:w-1/2">
-        <div class="shadow-md overflow-hidden rounded-lg bg-white hover:shadow-lg">
-          <a href="#" class="block">
-            <img src="https://i.imgur.com/YnGoLpP.jpg" class="w-20 h-20 object-cover block mx-auto"/>
-          </a>
+        <div class="flex w-max gap-6 justify-between cursor-pointer
+         items-center shadow-md overflow-hidden rounded-lg bg-white hover:shadow-lg">
+          <div href="#" class="block">
+            <img src="https://i.imgur.com/uzYifbO.jpg" class="w-10 h-10 object-cover block mx-auto"/>
+          </div>
 
-          <a href="#" class="block text-left text-base font-normal mt-2 ml-4">
-            Huawei Mate 50 Pro 512GB Orange
-          </a>
-
-          <h3 class="font-bold text-left text-lg mb-1 ml-4">$1,199.00 </h3>
+          <div href="#" class="text-base font-normal">
+            Iphone 13
+            <div class="text-sm">$1,799.00 X 1 </div>
+          </div>
+          <div class='font-light text-sm'>12 sep 2022<div class='font-medium text-red-500'>Pending</div></div>
         </div>
       </div>
+      <div class="px-2 my-2 sm:w-1/2">
+        <div class="flex w-max gap-6 justify-between cursor-pointer
+         items-center shadow-md overflow-hidden rounded-lg bg-white hover:shadow-lg">
+          <div href="#" class="block">
+            <img src="https://i.imgur.com/uzYifbO.jpg" class="w-10 h-10 object-cover block mx-auto"/>
+          </div>
 
-      <div class="w-full px-2 my-2 sm:w-1/2">
-        <div class="shadow-md overflow-hidden rounded-lg bg-white hover:shadow-lg">
-          <a href="#" class="block">
-            <img src="https://i.imgur.com/qx7UMAx.jpg" class="w-20 h-20 object-cover block mx-auto"/>
-          </a>
-
-          <a href="#" class="block text-left text-base font-normal mt-2 ml-4">
-            IPhone 14 pro Max 256GB Rose Gold
-          </a>
-
-          <h3 class="font-bold text-left text-lg mb-1 ml-4">$1,199.00 </h3>
+          <div href="#" class="text-base font-normal">
+            Iphone 13
+            <div class="text-sm">$1,799.00 X 1 </div>
+          </div>
+          <div class='font-light text-sm'>12 sep 2022<div class='font-medium text-green-500'>Completed</div></div>
         </div>
       </div>
-      <div class="w-full px-2 my-2 sm:w-1/2">
-        <div class="shadow-md overflow-hidden rounded-lg bg-white hover:shadow-lg">
-          <a href="#" class="block">
-            <img src="https://i.imgur.com/qx7UMAx.jpg" class="w-20 h-20 object-cover block mx-auto"/>
-          </a>
+      <div class="px-2 my-2 sm:w-1/2">
+        <div class="flex w-max gap-6 justify-between cursor-pointer
+         items-center shadow-md overflow-hidden rounded-lg bg-white hover:shadow-lg">
+          <div href="#" class="block">
+            <img src="https://i.imgur.com/uzYifbO.jpg" class="w-10 h-10 object-cover block mx-auto"/>
+          </div>
 
-          <a href="#" class="block text-left text-base font-normal mt-2 ml-4">
-            IPhone 14 pro Max 256GB Rose Gold
-          </a>
+          <div href="#" class="text-base font-normal">
+            Iphone 13
+            <div class="text-sm">$1,799.00 X 1 </div>
+          </div>
+          <div class='font-light text-sm'>12 sep 2022<div class='font-medium text-red-500'>Pending</div></div>
+        </div>
+      </div>
+      <div class="px-2 my-2 sm:w-1/2">
+        <div class="flex w-max gap-6 justify-between cursor-pointer
+         items-center shadow-md overflow-hidden rounded-lg bg-white hover:shadow-lg">
+          <div href="#" class="block">
+            <img src="https://i.imgur.com/uzYifbO.jpg" class="w-10 h-10 object-cover block mx-auto"/>
+          </div>
 
-          <h3 class="font-bold text-left text-lg mb-1 ml-4">$1,199.00 </h3>
+          <div href="#" class="text-base font-normal">
+            Iphone 13
+            <div class="text-sm">$1,799.00 X 1 </div>
+          </div>
+          <div class='font-light text-sm'>12 sep 2022<div class='font-medium text-red-500'>Pending</div></div>
+        </div>
+      </div>
+      <div class="px-2 my-2 sm:w-1/2">
+        <div class="flex w-max gap-6 justify-between
+         items-center shadow-md overflow-hidden rounded-lg bg-white hover:shadow-lg">
+          <div href="#" class="block">
+            <img src="https://i.imgur.com/uzYifbO.jpg" class="w-10 h-10 object-cover block mx-auto"/>
+          </div>
+
+          <div href="#" class="text-base font-normal">
+            Iphone 13
+            <div class="text-sm">$1,799.00 X 1 </div>
+          </div>
+          <div class='font-light text-sm'>12 sep 2022<div class='font-medium text-green-500'>Completed</div></div>
+        </div>
+      </div>
+      <div class="px-2 my-2 sm:w-1/2">
+        <div class="flex w-max gap-6 justify-between
+         items-center shadow-md overflow-hidden rounded-lg bg-white hover:shadow-lg">
+          <div href="#" class="block">
+            <img src="https://i.imgur.com/uzYifbO.jpg" class="w-10 h-10 object-cover block mx-auto"/>
+          </div>
+
+          <div href="#" class="text-base font-normal">
+            Iphone 13
+            <div class="text-sm">$1,799.00 X 1 </div>
+          </div>
+          <div class='font-light text-sm'>12 sep 2022<div class='font-medium text-red-500'>Pending</div></div>
+        </div>
+      </div>
+      <div class="px-2 my-2 sm:w-1/2">
+        <div class="flex w-max gap-6 justify-between
+         items-center shadow-md overflow-hidden rounded-lg bg-white hover:shadow-lg">
+          <div href="#" class="block">
+            <img src="https://i.imgur.com/uzYifbO.jpg" class="w-10 h-10 object-cover block mx-auto"/>
+          </div>
+
+          <div href="#" class="text-base font-normal">
+            Iphone 13
+            <div class="text-sm">$1,799.00 X 1 </div>
+          </div>
+          <div class='font-light text-sm'>12 sep 2022<div class='font-medium text-red-500'>Pending</div></div>
+        </div>
+      </div>
+      <div class="px-2 my-2 sm:w-1/2">
+        <div class="flex w-max cursor-pointer gap-6 justify-between
+         items-center shadow-md overflow-hidden rounded-lg bg-white hover:shadow-lg">
+          <div href="#" class="block">
+            <img src="https://i.imgur.com/uzYifbO.jpg" class="w-10 h-10 object-cover block mx-auto"/>
+          </div>
+
+          <div href="#" class="text-base font-normal">
+            Iphone 13
+            <div class="text-sm">$1,799.00 X 1 </div>
+          </div>
+          <div class='font-light text-sm'>12 sep 2022<div class='font-medium text-green-500'>Completed</div></div>
+        </div>
+      </div>
+      <div class="px-2 my-2 sm:w-1/2">
+        <div class="flex w-max gap-6 justify-between
+         items-center shadow-md overflow-hidden rounded-lg bg-white hover:shadow-lg">
+          <div href="#" class="block">
+            <img src="https://i.imgur.com/uzYifbO.jpg" class="w-10 h-10 object-cover block mx-auto"/>
+          </div>
+
+          <div href="#" class="text-base font-normal">
+            Iphone 13
+            <div class="text-sm">$1,799.00 X 1 </div>
+          </div>
+          <div class='font-light text-sm'>12 sep 2022<div class='font-medium text-red-500'>Pending</div></div>
+        </div>
+      </div>
+      <div class="px-2 my-2 sm:w-1/2">
+        <div class="flex w-max gap-6 justify-between
+         items-center shadow-md overflow-hidden rounded-lg bg-white hover:shadow-lg">
+          <div href="#" class="block">
+            <img src="https://i.imgur.com/uzYifbO.jpg" class="w-10 h-10 object-cover block mx-auto"/>
+          </div>
+
+          <div href="#" class="text-base font-normal">
+            Iphone 13
+            <div class="text-sm">$1,799.00 X 1 </div>
+          </div>
+          <div class='font-light text-sm'>12 sep 2022<div class='font-medium text-red-500'>Pending</div></div>
+        </div>
+      </div>
+      <div class="px-2 my-2 sm:w-1/2">
+        <div class="flex w-max gap-6 justify-between
+         items-center shadow-md overflow-hidden rounded-lg bg-white hover:shadow-lg">
+          <div href="#" class="block">
+            <img src="https://i.imgur.com/uzYifbO.jpg" class="w-10 h-10 object-cover block mx-auto"/>
+          </div>
+
+          <div href="#" class="text-base font-normal">
+            Iphone 13
+            <div class="text-sm">$1,799.00 X 1 </div>
+          </div>
+          <div class='font-light text-sm'>12 sep 2022<div class='font-medium text-red-500'>Pending</div></div>
+        </div>
+      </div>
+      <div class="px-2 my-2 sm:w-1/2">
+        <div class="flex w-max gap-6 justify-between
+         items-center shadow-md overflow-hidden rounded-lg bg-white hover:shadow-lg">
+          <div href="#" class="block">
+            <img src="https://i.imgur.com/uzYifbO.jpg" class="w-10 h-10 object-cover block mx-auto"/>
+          </div>
+
+          <div href="#" class="text-base font-normal">
+            Iphone 13
+            <div class="text-sm">$1,799.00 X 1 </div>
+          </div>
+          <div class='font-light text-sm'>12 sep 2022<div class='font-medium text-red-500'>Pending</div></div>
         </div>
       </div>
     </div>
